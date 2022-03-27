b946e3ba33405c81860ae2d19dc389473423173b
Rafael pena <rafael@goatme.app>
2022-03-27 03:51:07 -0300

    test: add test classe UserPositionRankingService

---
+++ tests/Unit/UserPositionRankingServiceTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use App\Services\UserPositionRankingService;
+
+class UserPositionRankingServiceTest extends TestCase
+{
+
+    public function test_should_set_position_to_object_from_array()
+    {
+        $sut = (new UserPositionRankingService($this->getRakingFake()))->execute();
+        $this->assertObjectHasAttribute('position', $sut[0]);
+        $this->assertEquals(10, $sut[0]->position);
+        $this->assertEquals(1, $sut[9]->position);
+    }
+
+    private function getRakingFake(): array
+    {
+        $ranking = [];
+        foreach (range(1, 10) as $i) {
+            $ranking[] = (object) [
+                'movementName' => 'Deadlift',
+                'userName' => 'Jose',
+                'userId' => $i,
+                'record' => $i,
+                'date' => '2021-01-06 00:00:00'
+            ];
+        }
+        return $ranking;
+    }
+}
