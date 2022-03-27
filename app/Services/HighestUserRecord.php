97905e0e70c2ea132452ebecaa3428941af55a5f
Rafael pena <rafael@goatme.app>
2022-03-27 02:15:03 -0300

    feat: creating class to get highest user record

---
+++ app/Services/HighestUserRecord.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services;
+
+class HighestUserRecord
+{
+    private array $ranking;
+
+    public function __construct(array $ranking)
+    {
+        $this->ranking = $ranking;
+        return $this;
+    }
+
+    public function execute(): array
+    {
+        $highestRecord = [];
+        foreach ($this->ranking as $r) {
+            if (array_key_exists($r->userId, $highestRecord)) {
+                if ($highestRecord[$r->userId]->record < $r->record) {
+                    $highestRecord[$r->userId] = $r;
+                }
+            } else {
+                $highestRecord[$r->userId] = $r;
+            }
+        }
+        return array_values($highestRecord);
+    }
+}
